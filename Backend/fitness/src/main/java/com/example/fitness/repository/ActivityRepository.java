a9c4c7cd0a0cb0b0e9a0453caebc335b286a04bc
Halima16 <105921782+Halima16@users.noreply.github.com>
2024-04-05 12:27:57 +0000

    Added Activity management

---
+++ Backend/fitness/src/main/java/com/example/fitness/repository/ActivityRepository.java
@@ -0,0 +1,9 @@
+package com.example.fitness.repository;
+import com.example.fitness.model.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ActivityRepository extends JpaRepository<Activity,Long>{
+
+
+}
