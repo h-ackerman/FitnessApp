38869bdd90236db02d5f8de7e24371b4bcc50495
Ali <Ali@gmail.com>
2024-04-18 15:04:33 +0100

    updated meal admin and added empty user front app and mobile app

---
+++ FrontAdmin/src/layouts/MenuList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Menu } from 'antd';
+import { HomeOutlined, PieChartOutlined, DatabaseOutlined, UserOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+
+export default function MenuList({ darkTheme }) {
+  const navigate = useNavigate();
+
+  const menuStyle = {
+    background: darkTheme ? '#5041BC' : '#ecb2ed', // Adjust background color based on dark mode
+    color: darkTheme ? '#FFFFFF' : '#000000', // Adjust text color based on dark mode
+  };
+
+  // These items will go to the Menu component from "antd" library
+  const menuItems = [
+    { key: 'home', icon: <PieChartOutlined />, label: 'Dashboard' },
+    { key: '/meal', icon: <DatabaseOutlined />, label: 'Meals' },
+    { key: '/activity', icon: <HomeOutlined />, label: 'Activity' },
+    { key: 'admin', icon: <UserOutlined />, label: 'Admin' },
+  ];
+
+  return (
+    // This menu will go to Sidebar component
+    <Menu
+      onClick={(e) => navigate(e.key)}
+      className='menu-bar'
+      theme={darkTheme ? 'dark' : 'light'}
+      style={menuStyle}
+      items={menuItems} // Use the `items` prop instead of `children`
+    />
+  );
+}
---
+++ FrontAdmin/src/meals/ViewMeal.jsx
@@ -0,0 +1,69 @@
+// This component shows when we click on view meal icon of a particular meal
+
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from 'react-bootstrap/Card';
+
+
+
+export default function ViewMeal(props) {
+  const [meal, setMeal] = useState({
+    name: "",
+    description: "",
+    calories: 0,
+    type: "",
+    protein: 0,
+    carbs: 0,
+    fats: 0,
+    image: "",
+
+  });
+  useEffect(() => {
+    const fetchMeal = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/meal/${props.id}`);
+        setMeal(response.data);
+      } catch (error) {
+        console.error("Error fetching meal:", error);
+        alert("An error occurred while fetching meal data. Please try again later.");
+      }
+    };
+
+    fetchMeal();
+  }, [props.id]);
+
+  return (
+    <div className="container">
+
+
+      <Card>
+        <div style={{ textAlign: 'center' }}>
+          <Card.Img src={meal.image} style={{ width: '250px', height: '250px' }} />
+        </div>
+
+        <Card.Body>
+          <Card.Text>
+            <b>Name:</b> {meal.name}
+            <br />
+            <b>Description:</b> {meal.description}
+            <br />
+            <b>Calories:</b> {meal.calories}
+            <br />
+            <b>Type:</b> {meal.type}
+            <br />
+            <b>Protein:</b> {meal.protein}
+            <br />
+            <b>Carbs:</b> {meal.carbs}
+            <br />
+            <b>Fats:</b> {meal.fats}
+          </Card.Text>
+
+        </Card.Body>
+      </Card>
+
+
+    </div>
+
+  );
+}
